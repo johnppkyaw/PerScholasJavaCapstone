67f1c765080c50db4e2aea0c890ef4571d8c6d6a
John Kyaw <jjpyae@gmail.com>
2024-03-16 19:27:43 -0700

    edit PatientRestController by adding ResponseEntity and @transactional

--- medmx/src/main/java/dev/johnkyaw/medmx/controller/PatientRestController.java
+++ medmx/src/main/java/dev/johnkyaw/medmx/controller/PatientRestController.java
@@ -4,7 +4,10 @@ import dev.johnkyaw.medmx.service.PatientServices;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import dev.johnkyaw.medmx.model.Patient;
 
@@ -22,41 +25,54 @@ public class PatientRestController {
         this.patientServices = patientServices;
     }
 
+    @Transactional(readOnly = true)
     @GetMapping("/physicians/{id}/patients")
-    public List<Patient> getAllPatientsByPhysician(@PathVariable("id") long id) {
-        return patientServices.getAllPatientsByPhysicianId(id);
+    public ResponseEntity<List<Patient>> getAllPatientsByPhysician(@PathVariable("id") long id) {
+        return ResponseEntity.ok(patientServices.getAllPatientsByPhysicianId(id));
     }
+
+    @Transactional
     @GetMapping("/patients")
-    public List<Patient> getAllPatients() {
-        return patientServices.getAllPatients();
+    public ResponseEntity<List<Patient>> getAllPatients() {
+        return ResponseEntity.ok(patientServices.getAllPatients());
     }
+
+    @Transactional
     @GetMapping("/patients/{id}")
-    public Optional<Patient> getPatientById(@PathVariable("id") long id) {
-        return patientServices.getPatientById(id);
+    public ResponseEntity<Optional<Patient>> getPatientById(@PathVariable("id") long id) {
+        return ResponseEntity.ok(patientServices.getPatientById(id));
     }
 
+    @Transactional
     @PostMapping("/patients")
-    public void createPatient(@RequestBody Patient patient) {
+    public ResponseEntity<Void> createPatient(@RequestBody Patient patient) {
         patientServices.addPatient(patient);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @Transactional
     @PutMapping("/patients/{id}")
-    public void updatePatient(@PathVariable("id") long id, @RequestBody Patient patient) {
+    public ResponseEntity<Void> updatePatient(@PathVariable("id") long id, @RequestBody Patient patient) {
         Optional<Patient> patientData = patientServices.getPatientById(id);
         if(patientData.isPresent()) {
             patientServices.updatePatient(id, patient);
+            return new ResponseEntity<>(HttpStatus.OK);
         } else {
             logger.warn("Attempted editing but the patient not found!");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
+    @Transactional
     @DeleteMapping("/patients/{id}")
-    public void deletePatient(@PathVariable("id") long id) {
+    public ResponseEntity<Void> deletePatient(@PathVariable("id") long id) {
         Optional<Patient> patientData = patientServices.getPatientById(id);
         if(patientData.isPresent()) {
             patientServices.deletePatient(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } else {
             logger.warn("Attempted editing but the patient not found!");
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 }
